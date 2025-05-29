a557167f23dfb9a04e78b7af02182c4112fe1a1f
Allison Barros <allisonramon@hotmail.com>
2025-05-29 18:26:14 -0300

    feat: first commit

---
+++ src/test/java/br/com/allidev/techevents/TecheventsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.allidev.techevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecheventsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
